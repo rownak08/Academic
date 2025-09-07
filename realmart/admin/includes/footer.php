7142ce80e5cd4c67b2f29156af3603b21992733f
rownak08 <107358370+rownak08@users.noreply.github.com>
2025-09-07 15:27:59 +0600

    Delete realmart directory

--- realmart/admin/includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>All rights reserved</b>
-    </div>
-    <strong>Copyright &copy; 2022 Brought To You By<a href="#">RealMart</a></strong>
-</footer>
